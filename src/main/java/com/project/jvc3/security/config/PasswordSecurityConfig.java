55cc7378f9d019637b5f6b3ec5914977f8cef053
박은빈 <joypeb@naver.com>
2023-04-23 19:21:52 +0900

    [dev] password 암호화 config작성

---
+++ src/main/java/com/project/jvc3/security/config/PasswordSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.project.jvc3.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
