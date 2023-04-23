93510eb25772c90c90dea598ccb2b4d75f7c0960
박은빈 <joypeb@naver.com>
2023-04-23 22:52:51 +0900

    [dev] User save, 이메일 인증 값 변경, 이메일 인증 one to one 작성

--- src/main/java/com/project/jvc3/domain/entity/User.java
+++ src/main/java/com/project/jvc3/domain/entity/User.java
@@ -1,5 +1,6 @@
 package com.project.jvc3.domain.entity;
 
+import com.project.jvc3.domain.dto.user.SignupRequest;
 import com.project.jvc3.domain.types.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -42,5 +43,26 @@ public class User {
     @Column(nullable = false)
     private LocalDateTime createdDate;
 
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
+    private EmailVerificationToken emailVerificationToken;
 
+    public static User save(SignupRequest signupRequest, String password) {
+        return User.builder()
+                .email(signupRequest.getEmail())
+                .name(signupRequest.getName())
+                .nickname(signupRequest.getNickname())
+                .password(password)
+                .userRole(UserRole.USER)
+                .emailVerified(false)
+                .createdDate(LocalDateTime.now())
+                .build();
+    }
+
+    public void verifyEmail() {
+        this.emailVerified = true;
+    }
+
+    public boolean getEmailVerified() {
+        return this.emailVerified;
+    }
 }
