f9fcf40dc6626b7136c92f5771ebd61d48e16335
박은빈 <joypeb@naver.com>
2023-04-23 22:51:32 +0900

    [dev] 이메일 전송, 이메일 확인 메서드 작성

---
+++ src/main/java/com/project/jvc3/service/EmailService.java
@@ -0,0 +1,79 @@
+package com.project.jvc3.service;
+
+import com.project.jvc3.domain.entity.EmailVerificationToken;
+import com.project.jvc3.domain.entity.User;
+import com.project.jvc3.exception.user.UserErrorCode;
+import com.project.jvc3.exception.user.UserException;
+import com.project.jvc3.repository.EmailVerificationTokenRepository;
+import com.project.jvc3.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final EmailVerificationTokenRepository emailVerificationTokenRepository;
+    private final UserRepository userRepository;
+
+    public void sendVerificationEmail(User user, String token) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setSubject("예수비전교회 이메일 인증");
+        mailMessage.setText("회원가입을 마치려면 다음 링크를 통해 인증을 받아주세요: "
+                + "http://localhost:8080/api/v1/users/verify-email?token=" + token);
+        javaMailSender.send(mailMessage);
+    }
+
+    @Async("taskExecutor")
+    public void getEmailToken(User user) {
+        //토큰, 토큰만료시간 추가
+        String token = UUID.randomUUID().toString();
+        LocalDateTime expiryDate = LocalDateTime.now().minusHours(24);
+
+        //토큰 객체 생성
+        EmailVerificationToken emailVerificationToken = EmailVerificationToken.save(user,token,expiryDate);
+
+        //토큰 저장
+        emailVerificationTokenRepository.save(emailVerificationToken);
+
+        //이메일 전송
+        sendVerificationEmail(user, emailVerificationToken.getToken());
+    }
+
+    public void verifyEmail(String token) {
+        //토큰을 이용해 객체 확인
+        EmailVerificationToken emailVerificationToken = emailVerificationTokenRepository.findByToken(token);
+
+        log.info(emailVerificationToken.getUser().getEmail());
+
+        //객체가 존재하지 않을경우
+        if (emailVerificationToken == null) {
+            throw new UserException(UserErrorCode.INVALID_VERIFICATION_EMAIL_TOKEN);
+        }
+
+        //토큰 기간이 만료되었을경우
+        if (!emailVerificationToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+            throw new UserException(UserErrorCode.EMAIL_VERIFICATION_TOKEN_EXPIRED);
+        }
+
+        //유저가 존재하지 않을경우
+        User user = userRepository.findById(emailVerificationToken.getUser().getId()).orElseThrow(() -> {
+            throw new UserException(UserErrorCode.USER_NOT_FOUND);
+        });
+
+        //이메일 인증 성공
+        user.verifyEmail();
+        userRepository.save(user);
+    }
+}
