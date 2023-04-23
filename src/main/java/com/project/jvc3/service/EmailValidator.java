345948c2bec390392fc29c5702f9d39dc6e615fe
박은빈 <joypeb@naver.com>
2023-04-23 22:51:05 +0900

    [dev] 이메일 유효성 검증 클래스

---
+++ src/main/java/com/project/jvc3/service/EmailValidator.java
@@ -0,0 +1,15 @@
+package com.project.jvc3.service;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    private static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+}
