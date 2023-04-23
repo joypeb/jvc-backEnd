16de80fabb5c2ee9add3a5b2fe9b25f9110dfcf3
박은빈 <joypeb@naver.com>
2023-04-23 16:31:45 +0900

    [build] first build

---
+++ src/main/java/com/project/jvc3/Jvc3Application.java
@@ -0,0 +1,13 @@
+package com.project.jvc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jvc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jvc3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/jvc3/Jvc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jvc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jvc3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
