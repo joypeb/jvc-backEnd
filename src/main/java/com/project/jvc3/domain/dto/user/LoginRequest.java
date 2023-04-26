994aae32d1b0195be66f34176d3441ac4ec786e0
박은빈 <joypeb@naver.com>
2023-04-26 17:38:40 +0900

    [dev] LoginRequest 생성자 추가

--- src/main/java/com/project/jvc3/domain/dto/user/LoginRequest.java
+++ src/main/java/com/project/jvc3/domain/dto/user/LoginRequest.java
@@ -1,10 +1,14 @@
 package com.project.jvc3.domain.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
