467c178ccdcc306f0e3615db4fe6deaa1825b351
박은빈 <joypeb@naver.com>
2023-04-26 17:22:19 +0900

    [dev] email을 통해 Optional EmailVerificationToken를 가져오는 쿼리메서드 추가

--- src/main/java/com/project/jvc3/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/project/jvc3/repository/EmailVerificationTokenRepository.java
@@ -4,7 +4,11 @@ import com.project.jvc3.domain.entity.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
     EmailVerificationToken findByToken(String token);
+
+    Optional<EmailVerificationToken> findByUserEmail(String email);
 }
