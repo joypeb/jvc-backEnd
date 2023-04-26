a69eae782f767ed64d12d26d389cc2a43b4ec463
박은빈 <joypeb@naver.com>
2023-04-26 17:22:38 +0900

    [dev] signupRequest 변수 순서 변경

--- src/main/java/com/project/jvc3/domain/dto/user/SignupRequest.java
+++ src/main/java/com/project/jvc3/domain/dto/user/SignupRequest.java
@@ -1,13 +1,17 @@
 package com.project.jvc3.domain.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequest {
     private String email;
+    private String password;
     private String name;
     private String nickname;
-    private String password;
 }
