5351b4b6de1eb35772699d2937d835f466977998
박은빈 <joypeb@naver.com>
2023-04-26 17:39:12 +0900

    [test] login 성공, 패스워드 실패, 이메일 인증 실패 테스트케이스 통과

--- src/test/java/com/project/jvc3/controller/UserControllerTest.java
+++ src/test/java/com/project/jvc3/controller/UserControllerTest.java
@@ -2,13 +2,16 @@ package com.project.jvc3.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.jvc3.domain.dto.user.LoginRequest;
 import com.project.jvc3.domain.dto.user.SignupRequest;
 import com.project.jvc3.domain.entity.EmailVerificationToken;
 import com.project.jvc3.domain.entity.User;
+import com.project.jvc3.domain.types.UserRole;
 import com.project.jvc3.repository.EmailVerificationTokenRepository;
 import com.project.jvc3.repository.UserRepository;
 import com.project.jvc3.security.key.KeyGenerator;
 import com.project.jvc3.security.key.KeyStore;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
@@ -173,6 +177,85 @@ class UserControllerTest {
 
         mockMvc.perform(get(BASE_URL + "/verify-email")
                         .param("token",emailVerificationToken.getToken()))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    public void testLogin_success() throws Exception{
+        //회원가입
+        SignupRequest signupRequest = new SignupRequest("test@test.co","password","name","nickname");
+
+        mockMvc.perform(post(BASE_URL + "/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk());
+
+        //이메일 인증 처리
+        User user = userRepository.findByEmail(signupRequest.getEmail()).get();
+        user.verifyEmail();
+        userRepository.save(user);
+
+        LoginRequest loginRequest = new LoginRequest(signupRequest.getEmail(), signupRequest.getPassword());
+
+        MvcResult result = mockMvc.perform(post(BASE_URL + "/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        //쿠키 발급이 됐는지 확인
+        Cookie[] cookies = result.getResponse().getCookies();
+
+        assertEquals(2, cookies.length);
+        assertEquals("access_token", cookies[0].getName());
+        assertEquals("refresh_token", cookies[1].getName());
+        assertTrue(cookies[0].isHttpOnly());
+        assertTrue(cookies[0].getMaxAge() > 0);
+        assertTrue(cookies[1].isHttpOnly());
+        assertTrue(cookies[1].getMaxAge() > 0);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 패스워드 불일치")
+    public void testLogin_failed_password() throws Exception{
+        //회원가입
+        SignupRequest signupRequest = new SignupRequest("test@test.co","password","name","nickname");
+
+        mockMvc.perform(post(BASE_URL + "/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk());
+
+        //이메일 인증 처리
+        User user = userRepository.findByEmail(signupRequest.getEmail()).get();
+        user.verifyEmail();
+        userRepository.save(user);
+
+        LoginRequest loginRequest = new LoginRequest(signupRequest.getEmail(), signupRequest.getPassword() + "1");
+
+        mockMvc.perform(post(BASE_URL + "/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 이메일 인증 미완료")
+    public void testLogin_failed_emailVerificationInvalid() throws Exception{
+        //회원가입
+        SignupRequest signupRequest = new SignupRequest("test@test.co","password","name","nickname");
+
+        mockMvc.perform(post(BASE_URL + "/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk());
+
+        LoginRequest loginRequest = new LoginRequest(signupRequest.getEmail(), signupRequest.getPassword());
+
+        mockMvc.perform(post(BASE_URL + "/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
                 .andExpect(status().isBadRequest());
     }
 }
