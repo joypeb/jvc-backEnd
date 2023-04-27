50f3ae54d65ff0b63102c61eef5b475ac5321244
박은빈 <joypeb@naver.com>
2023-04-27 23:11:24 +0900

    [dev] security config requestMatchers변경, jwt 검증 및 예외처리 로직 추가

--- src/main/java/com/project/jvc3/security/config/SecurityConfig.java
+++ src/main/java/com/project/jvc3/security/config/SecurityConfig.java
@@ -1,18 +1,28 @@
 package com.project.jvc3.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.jvc3.security.jwt.JwtExceptionHandler;
+import com.project.jvc3.security.jwt.JwtFilter;
+import com.project.jvc3.security.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig{
+
+    private final JwtUtils jwtUtils;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,13 +30,16 @@ public class SecurityConfig{
                 .csrf().disable()
                 .httpBasic().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/api/v1/users/**").permitAll()
+                .requestMatchers(new AntPathRequestMatcher( "/swagger-ui/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher( "/v3/api-docs/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher( "/api/v1/users/**")).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .build();
+                .addFilterBefore(new JwtFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionHandler(objectMapper), JwtFilter.class)
+                .getOrBuild();
     }
 }
