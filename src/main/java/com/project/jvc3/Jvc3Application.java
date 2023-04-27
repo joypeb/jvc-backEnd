7d8d5201fca9db5f54c656d896aaac8aff36a2be
박은빈 <joypeb@naver.com>
2023-04-27 23:09:02 +0900

    [dev] auditing 추가

--- src/main/java/com/project/jvc3/Jvc3Application.java
+++ src/main/java/com/project/jvc3/Jvc3Application.java
@@ -5,12 +5,14 @@ import com.project.jvc3.security.key.KeyGenerator;
 import com.project.jvc3.security.key.KeyStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyPair;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Jvc3Application {
 
